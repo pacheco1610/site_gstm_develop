1f2fe22ef37198c2ee6156841af27dee44edb37d
Noe Aviles <noe.alexander.aviles@gmail.com>
2023-08-17 23:15:59 -0600

    Se comienza con la maquetacion seccion about

--- src/index.js
+++ src/index.js
@@ -7,12 +7,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from './pages/home'
+import About from './pages/about'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home/>,
+  },  {
+    path: "/about",
+    element: <About/>,
   },
+  
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
