831a5f695bf0dec620b6d90e8df3566e4028bfd7
Noe Aviles <noe.alexander.aviles@gmail.com>
2023-08-19 14:03:23 -0600

    Se termina seccion about

---
+++ src/components/footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import logo from '../assets/logo-2.png';
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className="footer-container">
+        <div className="footer-logo">
+          <img src={logo} alt="" />
+        </div>
+          <nav className='footer-nav'>
+            <ul>
+              <li>Nosotros</li>
+              <li><a href="">A cerca de</a></li>
+              <li><a href="">Bolsa de Trabajo</a></li>
+              <li><a href="">Aviso de Confidencialidad</a></li>
+            </ul>
+          </nav>
+        <div className="footer-redes">
+          <span>Siguenos:</span>
+          <div>
+          <i class="fa-brands fa-facebook-f"></i>
+          <i class="fa-brands fa-instagram"></i>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
-
+import imagenslider from '../../src/assets/imagenslider2.png';
 const Gallery = () => {
   return (
-    <section className='gallery'>
-    <div className="gallery-img"><img src="" alt="" /></div>
-    <div className="gallery-img"><img src="" alt="" /></div>
-    <div className="gallery-img"><img src="" alt="" /></div>
-    <div className="gallery-img"><img src="" alt="" /></div>
-    <div className="gallery-img"><img src="" alt="" /></div>
-    <div className="gallery-img"><img src="" alt="" /></div>
-    <div className="gallery-img"><img src="" alt="" /></div>
-    <div className="gallery-img"><img src="" alt="" /></div>
-    <div className="gallery-img"><img src="" alt="" /></div>
+    <section className='gallery '>
+        <div className="InfoAbout-media">
+            <div className="InfoAbout-video"><img src={imagenslider} alt="" /></div>
+            <div className="InfoAbout-container">
+                <div className="InfoAbout-pictures">
+                    <div className="InfoAbout-picture"><img src={imagenslider} alt="" /></div>
+                    <div className="InfoAbout-picture"><img src={imagenslider} alt="" /></div>
+                </div>
+                <div className="InfoAbout-pictures">
+                    <div className="InfoAbout-picture"><img src={imagenslider} alt="" /></div>
+                    <div className="InfoAbout-picture"><img src={imagenslider} alt="" /></div>
+                </div>
+            </div>
+        </div>
+        <div className="InfoAbout-media">
+            <div className="InfoAbout-container">
+                <div className="InfoAbout-pictures">
+                    <div className="InfoAbout-picture"><img src={imagenslider} alt="" /></div>
+                    <div className="InfoAbout-picture"><img src={imagenslider} alt="" /></div>
+                </div>
+            </div>
+            <div className="InfoAbout-container">
+                <div className="InfoAbout-pictures">
+                    <div className="InfoAbout-picture"><img src={imagenslider} alt="" /></div>
+                    <div className="InfoAbout-picture"><img src={imagenslider} alt="" /></div>
+                </div>
+            </div>
+        </div>
   </section>
   )
 }
